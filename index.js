64ff318b7bbe389dcb0c5587ec2ea7212ba85f07
Aksha-40007 <akshavad40007@gmail.com>
2024-03-22 14:12:30 +0530

    changes in health route res

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const verifyUser = require("./src/middleware/authMiddleware");
 
 app.use(cors());
 app.use(express.json());  
-app.use(express.static(path.join(__dirname, 'public')));
-app.get("/health", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/health",(req,res)=>{
+    res.json({
+        service:"Job listing server",
+        status:"Active"
+    });
 });
 
 
