9d235c1dabc7a4bb8be4b36c0d027b825a91936c
Aksha-40007 <akshavad40007@gmail.com>
2024-03-30 00:36:05 +0530

    fixing cors error

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const orderRoutes= require("./src/routes/orderRoutes");
 const feedbackRoutes= require("./src/routes/feedbackRoute");
 const verifyUser = require("./src/middleware/authMiddleware");
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (origin) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-
+// app.use((req, res, next) => {
+//     const origin = req.headers.origin;
+//     if (origin) {
+//       res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
+app.use(cors());
 app.use(express.json());  
 app.get("/health",(req,res)=>{
     res.json({
