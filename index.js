7ed739f2e6f8e6ec40afd16645f0499cfcaa4908
Aksha-40007 <akshavad40007@gmail.com>
2024-03-29 21:13:02 +0530

    added cors headers

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ const feedbackRoutes= require("./src/routes/feedbackRoute");
 const verifyUser = require("./src/middleware/authMiddleware");
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    const origin = req.headers.origin;
+    if (origin) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
-
-  app.options('*', (req, res) => {
-    res.status(200).end();
   });
 
 app.use(express.json());  
